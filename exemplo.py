8ebca22c550c192e4780cd53dfdd9ae957311995
JhessicaBrumGuimaraes <163341216+JhessicaBrumGuimaraes@users.noreply.github.com>
2024-03-20 16:01:31 -0300

    Add files via upload

---
+++ exemplo.py
@@ -0,0 +1,39 @@
+print("Jhessica")
+
+
+numero = 10
+nome = "Jhessica" 
+booleano = True 
+
+numero_real = 10.0
+
+
+# A ordem de precêdencia dos operadores lógicos segue a seguinte sequência: not, and e or.
+#a modificação da ordem desses operadoras, só é possível com o uso de ()
+
+a = 15
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+
+resposta  = (6 + 8 == 14) or (2 * 7 and 28 / 2)
+
+
+c = True
+d = False
+resultado1 = c and d
+resultado2 = c or d
+resultado3 = not c
+
+c = 3
+c == 4
+c > 2
+c != 3
+d = 4
+c is d
+c is not d
+
+ 
+respostaa =  (c == 5 < 4 ) or (c != 6 and c is d)
